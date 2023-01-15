c2d7d7862ff10b00da6020064aad18101e3cff53
OlenaLutsenko <olena.lutsenko.qa@gmail.com>
2023-01-15 17:26:46 +0200

    mend
    create app

---
+++ explanation.js
@@ -0,0 +1,3 @@
+exports.explanation = `
+Why this approach works? We called it DEPRESSIVE CONSULTING FOR DEPRESSIVE PEOPLE OR PEOPLE IN BAD MOOD. If person is already depressive, the positive recomendations can seem to them irrelevant. It seems strange that some people advise them something for healing state about what they don\'t know.
+`;
